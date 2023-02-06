0dfde26f55abf52de75c567e6a2062e035103ff5
Adriana Ochoa <adrianaochoa.dev@gmail.com>
2023-02-05 22:13:19 -0700

    saves profile name

--- screens/EditName.tsx
+++ screens/EditName.tsx
@@ -1,12 +1,42 @@
-import { StyleSheet, View } from "react-native";
+import { useEffect } from "react";
+import { Alert, StyleSheet, ToastAndroid, View } from "react-native";
 import { Button, FormInput, moveToBottom, useFormInput } from "../components";
+import { useAsyncStorage } from "../hooks/useAsyncStorage";
+import { SettingsStackScreenProps } from "../types";
 
-export function EditName() {
+export function EditName({ navigation }: SettingsStackScreenProps<"Settings">) {
   const { onChangeText, value } = useFormInput();
+  const [profileName, setProfileName] = useAsyncStorage("profileName", "");
+
+  const onSave = () => {
+    Alert.alert("Confirm", "Change profile name?", [
+      {
+        text: "Cancel",
+      },
+      {
+        text: "OK",
+        onPress: () => {
+          if (value !== profileName) setProfileName(value);
+          ToastAndroid.show("Profile name updated", ToastAndroid.SHORT);
+        },
+      },
+    ]);
+  };
+
+  const onCancel = () => {
+    navigation.navigate("Settings");
+  };
+
+  useEffect(() => {
+    if (profileName) {
+      onChangeText(profileName);
+    }
+  }, [profileName]);
 
   return (
     <View style={styles.container}>
       <FormInput
+        autoFocus
         placeholder="Profile name"
         onChangeText={onChangeText}
         value={value}
@@ -14,10 +44,10 @@ export function EditName() {
 
       {moveToBottom(
         <View style={styles.buttonsContainer}>
-          <Button variant="outlined" onPress={() => {}}>
+          <Button variant="outlined" onPress={onCancel}>
             Cancel
           </Button>
-          <Button style={{ marginLeft: 20 }} onPress={() => {}}>
+          <Button style={{ marginLeft: 20 }} onPress={onSave}>
             Save
           </Button>
         </View>
