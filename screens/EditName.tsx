b4f6579304a6ce7019b83105c04b07aa0643c578
Adriana Ochoa <adrianaochoa.dev@gmail.com>
2023-02-05 21:40:54 -0700

    add EditName screen

--- screens/EditName.tsx
+++ screens/EditName.tsx
@@ -1,7 +1,39 @@
-import { View } from 'react-native'
+import { StyleSheet, View } from "react-native";
+import { Button, FormInput, moveToBottom, useFormInput } from "../components";
 
-export function EditName () {
+export function EditName() {
+  const { onChangeText, value } = useFormInput();
+
   return (
-    <View>'EditName'</View>
-  )
+    <View style={styles.container}>
+      <FormInput
+        placeholder="Profile name"
+        onChangeText={onChangeText}
+        value={value}
+      />
+
+      {moveToBottom(
+        <View style={styles.buttonsContainer}>
+          <Button variant="outlined" onPress={() => {}}>
+            Cancel
+          </Button>
+          <Button style={{ marginLeft: 20 }} onPress={() => {}}>
+            Save
+          </Button>
+        </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f6f6f6",
+    paddingTop: 20,
+    flex: 1,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+});
