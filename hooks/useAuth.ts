025a27b181e23c36950445eb06685e3774596ef3
Adriana Ochoa <adrianaochoa.dev@gmail.com>
2023-02-06 20:31:18 -0700

    add auth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { Alert, ToastAndroid } from "react-native";
+import { useAsyncStorage } from "./useAsyncStorage";
+import * as LocalAuthentication from "expo-local-authentication";
+
+export const useAuth = ()=>{
+    const [biometricsSupported, setBiometricsSupported] = useAsyncStorage(
+        "biometricsSupported",
+        ""
+      );
+
+      const askForPassword = (action: () => void) => {
+        Alert.prompt("Ingrese su contraseña", undefined, (password) => {
+          if (password === "yourpassword") {
+            ToastAndroid.show(
+              "Autenticación con contraseña exitosa",
+              ToastAndroid.SHORT
+            );
+            action();
+          } else {
+            ToastAndroid.show(
+              "Autenticación con contraseña fallida",
+              ToastAndroid.SHORT
+            );
+          }
+        });
+      };
+    
+      const askForBiometrics = async (action: () => void) => {
+        if (!biometricsSupported) {
+          askForPassword(action);
+          return;
+        }
+    
+        try {
+          const result = await LocalAuthentication.authenticateAsync({
+            promptMessage: "Autenticación necesaria",
+          });
+    
+          if (result.success) {
+            ToastAndroid.show("Autenticación exitosa", ToastAndroid.SHORT);
+            action();
+          }
+        } catch (error) {
+          ToastAndroid.show("Autenticación fallida", ToastAndroid.SHORT);
+        }
+      };
+
+
+      return {
+        askForPassword,
+        askForBiometrics
+      }
+
+}
